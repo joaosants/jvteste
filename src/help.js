02b1b1cb374f2adad0bf6f4dac3b8cc08e90559f
joaosants <joaosantos5926y@gmail.com>
2021-02-08 17:05:51 -0400

    First commit

---
+++ src/help.js
@@ -0,0 +1,99 @@
+const help = (prefix) => {
+	return `
+「 *jão BOT* 」
+
+◪ *informações*
+  ❏ prefixo: 「  *  」
+
+  ❏ criador: jão
+
+  ❏ meu insta:https://instagram.com/jaoffx?igshid=1aqzc3oxkp79l
+
+
+
+ ◪ *FIGURINHAS*
+
+  ├─ ❏ ${prefix}sticker converte foto em figurinha
+  ├─ ❏ ${prefix}stickergif - converte gif em figurinha (se for vídeo max 10 seg)
+  ├─ ❏ ${prefix}toimg converte figurinha em imagem 
+  └─ ❏ caso n chegue algo envie dnv
+  
+
+◪ *SOM*
+
+  ├─ ❏ ${prefix}play e o nome da música faz o bot baixar a musica
+  ├─ ❏ ${prefix}tts pt e a frase faz o bot gravar áudio
+  └─ ❏ caso n chegue algo envie dnv
+
+
+◪ *DOWNLOADER*
+
+  ├─ ❏ ${prefix}images (link) o bot baixa o link te envia a imagem
+  ├─ ❏ ${prefix}ytmp3 (link) o bot baixa o link e te envia em música 
+  ├─ ❏ ${prefix}ytmp4 (link) o bot baixa o link e te envia em vídeo
+  ├─ ❏ ${prefix}tiktok (link) o bot baixa o link e te envia o vídeo 
+  └─ ❏ caso n chegue algo envie dnv
+
+
+◪ *STALKEAR*
+
+  ├─ ❏ ${prefix}tiktokstalk (nome do perfil)
+  ├─ ❏ ${prefix}igstalk (nome do perfil)
+  └─ ❏ caso n chegue algo envie dnv
+
+
+◪ *ANIME*
+
+  ├─ ❏ ${prefix}neonime o bot envia uma foto de neonime aleatório
+  ├─ ❏ ${prefix}pokemon o bot envia uma foto de pokémon aleatório
+  ├─ ❏ ${prefix}loli o bot envia uma foto de loli aleatória
+  ├─ ❏ ${prefix}randomanime te da um anime aleatório
+  ├─ ❏ ${prefix}wait descobre qual anime é pela foto enviada
+  └─ ❏ caso n chegue algo envie dnv
+
+
+◪ *TEXTOS*
+
+  ├─ ❏ ${prefix}bpink (seu txt)
+  ├─ ❏ ${prefix}marvellogo (seu txt)
+  ├─ ❏ ${prefix}snowwrite (seu txt)
+  ├─ ❏ ${prefix}3dtext (seu txt)
+  ├─ ❏ ${prefix}ninjalogo (seu txt)
+  ├─ ❏ ${prefix}water (seu txt)
+  ├─ ❏ ${prefix}firetext (seu txt) 
+  ├─ ❏ ${prefix}logowolf (seu txt)
+  ├─ ❏ ${prefix}logowolf2 (seu txt)
+  ├─ ❏ ${prefix}phlogo (seu txt)
+  ├─ ❏ ${prefix}glitch (seu txt)
+  ├─ ❏ ${prefix}neonlogo (seu txt)
+  ├─ ❏ ${prefix}neonlogo2 (seu txt)
+  ├─ ❏ ${prefix}lionlogo (seu txt)
+  ├─ ❏ ${prefix}jokerlogo (seu txt)
+  ├─ ❏ ${prefix}shadow (seu txt)
+  ├─ ❏ ${prefix}burnpaper (seu txt)
+  ├─ ❏ ${prefix}coffee (seu txt)
+  ├─ ❏ ${prefix}lovepaper (seu txt) 
+  ├─ ❏ ${prefix}woodblock (seu txt) 
+  ├─ ❏ ${prefix}qowheart (seu txt) 
+  ├─ ❏ ${prefix}mutgrass (seu txt)
+  ├─ ❏ ${prefix}undergocean (seu txt)
+  ├─ ❏ ${prefix}woodenboards (seu txt)
+  ├─ ❏ ${prefix}wolfmetal (seu txt)
+  ├─ ❏ ${prefix}metalictglow (seu txt)
+  ├─ ❏ ${prefix}8bit (seu txt)
+  ├─ ❏ ${prefix}ttp (seu txt)
+  ├─ ❏ ${prefix}herrypotter (seu txt)
+  ├─ ❏ ${prefix}pubglogo (seu txt)
+  └─ ❏ caso n chegue algo envie dnv
+
+
+ㅤㅤㅤㅤ║▌│█║▌│ █║▌│█│║▌║
+ㅤㅤㅤㅤ║▌│█║▌│ █║▌│█│║▌║
+
+ㅤㅤCopyright © jão 2021
+
+
+`
+}
+
+exports.help = help
